2008e97aa8f489cafeb136828b25d8ad849eae6d
Aviral Dasgupta <aviraldg@gmail.com>
2012-03-28 21:55:11 +0530

    Reorganized URLs and views

--- quizzardous/urls.py
+++ quizzardous/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic.simple import redirect_to
+from django.core.urlresolvers import reverse
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^$', 'quizzardous.questions.views.questions', name='questions'),
+	# TODO: This, the homepage, should in the future point to a list of the
+	# most popular recent questions. For now, this should do.
+	(r'^$', redirect_to, {'url': 'questions'}),
+
+	(r'^questions/', include('quizzardous.questions.urls'))
 )
 
 # only used in development environment (to serve static files)
