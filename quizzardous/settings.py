d2c86923060f8907453a1fd8f0dff100e4c6460b
Aviral Dasgupta <aviraldg@gmail.com>
2012-04-06 14:01:47 +0530

    Added django-crispy-forms to requirements.

--- quizzardous/settings.py
+++ quizzardous/settings.py
@@ -132,6 +132,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gunicorn',
+    'crispy_forms',
     'questions',
     'users',
     # Uncomment the next line to enable the admin:
