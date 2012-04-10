651972be3130fc6f1c9fedfea82683f9a86c12fb
Aviral Dasgupta <aviraldg@gmail.com>
2012-04-11 00:22:44 +0530

    Changed slugify to convert multiple spaces to one dash only.

--- quizzardous/utils.py
+++ quizzardous/utils.py
@@ -4,6 +4,7 @@ import re
 def slugify(value):
     value = unicode(value)
     value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
-    value = unicode(re.sub(' ', '-', re.sub('[^\w\s-]', '', value).strip().lower()))
+    value = re.sub(' ', '-', re.sub('[^\w\s-]', '', value).strip().lower())
+    value = unicode(re.sub('-+', '-', value))
     # slugs should be a maximum of fifty characters long
     return value[:50]
